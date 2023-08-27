3b29d9644a8f3641aa84c587e861154224e59fb9
Mark Prettyman <markiesmyname@Marks-iMac.lan>
2023-08-27 08:54:29 -0700

    import export React

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+import NavBar from "./NavBar.js";
+import Home from "./Home.js";
+import About from "./About.js";
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      <Home />
-      <About />
-    </div>
-  );
+    return (
+        <div>
+            <NavBar />
+            <Home />
+            <About />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { username } from "../data/user.js";
+import { city } from "../data/user.js";
 
 function Home() {
-  return (
-    <div id="home">
-      <h1>
-        {username} is a Web Developer from {city}
-      </h1>
-    </div>
-  );
+    return (
+        <div id="home">
+            <h1>
+                {username} is a Web Developer from {city}
+            </h1>
+        </div>
+    );
 }
+
+export default Home;
